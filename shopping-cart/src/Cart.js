35e6da575dfd625c82010b7cb6c3d0d0c44d6e88
ilgoncalves <lima-igor@hotmail.com>
2022-03-15 01:45:09 -0300

    search before to add item to ensure there arent repeated

--- shopping-cart/src/Cart.js
+++ shopping-cart/src/Cart.js
@@ -1,8 +1,14 @@
 export default class Cart {
   items = [];
+
   add(item) {
+    const indexToRemove = this.items.findIndex((el) => el.name === item.name);
+    if (indexToRemove > -1) {
+      this.items.splice(indexToRemove, 1);
+    }
     this.items.push(item);
   }
+
   getTotal() {
     return this.items.reduce(
       (acc, element) => (acc += element.product.price * element.quantity),
--- shopping-cart/src/Cart.spec.js
+++ shopping-cart/src/Cart.spec.js
@@ -3,6 +3,11 @@ import Cart from "./Cart";
 describe("Test Cart class", () => {
   let cart;
 
+  let product = {
+    name: "sunglass",
+    price: 1050, // R$10,50
+  };
+
   beforeEach(() => {
     cart = new Cart();
   });
@@ -13,15 +18,42 @@ describe("Test Cart class", () => {
 
   it("Should add a item to the cart and then get the total", () => {
     const item = {
-      product: {
-        name: "sunglass",
-        price: 1050, //R$ 10,50
-      },
+      product,
       quantity: 3,
     };
 
     cart.add(item);
 
     expect(cart.getTotal()).toEqual(3150);
+  });
+
+  it("Should unsure to keep the last add if pass the same product", () => {
+    cart.add({
+      product,
+      quantity: 3,
+    });
+    cart.add({
+      product,
+      quantity: 1,
+    });
+
+    expect(cart.getTotal()).toEqual(1050);
+  });
+
+  it("Should unsure to keep the last add if pass the same product adding 3 times", () => {
+    cart.add({
+      product,
+      quantity: 3,
+    });
+    cart.add({
+      product,
+      quantity: 1,
+    });
+    cart.add({
+      product,
+      quantity: 4,
+    });
+
+    expect(cart.getTotal()).toEqual(1050 * 4);
   });
 });
