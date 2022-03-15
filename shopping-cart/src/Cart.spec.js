2b70412fd94d0da3de375fce4d50c7f55cf9005d
ilgoncalves <lima-igor@hotmail.com>
2022-03-15 11:26:16 -0300

    test: create test to checkout function

--- shopping-cart/src/Cart.spec.js
+++ shopping-cart/src/Cart.spec.js
@@ -1,6 +1,6 @@
 import Cart from "./Cart";
 
-describe("Test Cart class", () => {
+describe("test Cart class", () => {
   let cart;
 
   let product = {
@@ -17,63 +17,80 @@ describe("Test Cart class", () => {
     cart = new Cart();
   });
 
-  it("Should receive 0 when call getTotal after instanciate a new Cart", () => {
-    expect(cart.getTotal()).toEqual(0);
+  describe("tests on getTotal", () => {
+    it("Should receive 0 when call getTotal after instanciate a new Cart", () => {
+      expect(cart.getTotal()).toEqual(0);
+    });
+
+    it("Should add a item to the cart and then get the total", () => {
+      const item = {
+        product,
+        quantity: 3,
+      };
+
+      cart.add(item);
+
+      expect(cart.getTotal()).toEqual(product.price * item.quantity);
+    });
+
+    it("Should unsure to keep the last add if pass the same product", () => {
+      cart.add({
+        product,
+        quantity: 3,
+      });
+      cart.add({
+        product,
+        quantity: 1,
+      });
+
+      expect(cart.getTotal()).toEqual(product.price);
+    });
+
+    it("Should ensure to keep the last add if pass the same product adding 3 times", () => {
+      cart.add({
+        product,
+        quantity: 3,
+      });
+      cart.add({
+        product,
+        quantity: 1,
+      });
+      cart.add({
+        product,
+        quantity: 4,
+      });
+
+      expect(cart.getTotal()).toEqual(product.price * 4);
+    });
+
+    it("should ensure that the value will be updated when add and then remove a item to cart", () => {
+      cart.add({
+        product,
+        quantity: 3,
+      });
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
+
+      cart.remove(product);
+
+      expect(cart.getTotal()).toEqual(product2.price);
+    });
   });
 
-  it("Should add a item to the cart and then get the total", () => {
-    const item = {
-      product,
-      quantity: 3,
-    };
+  describe("tests on checkout", () => {
+    it("should brign the total and the items when call checkout", () => {
+      cart.add({
+        product,
+        quantity: 5,
+      });
+      cart.add({
+        product: product2,
+        quantity: 1,
+      });
 
-    cart.add(item);
-
-    expect(cart.getTotal()).toEqual(product.price * item.quantity);
-  });
-
-  it("Should unsure to keep the last add if pass the same product", () => {
-    cart.add({
-      product,
-      quantity: 3,
+      expect(cart.checkout()).toMatchSnapshot();
     });
-    cart.add({
-      product,
-      quantity: 1,
-    });
-
-    expect(cart.getTotal()).toEqual(product.price);
-  });
-
-  it("Should ensure to keep the last add if pass the same product adding 3 times", () => {
-    cart.add({
-      product,
-      quantity: 3,
-    });
-    cart.add({
-      product,
-      quantity: 1,
-    });
-    cart.add({
-      product,
-      quantity: 4,
-    });
-
-    expect(cart.getTotal()).toEqual(product.price * 4);
-  });
-
-  it("should ensure that the value will be updated when add and then remove a item to cart", () => {
-    cart.add({
-      product,
-      quantity: 3,
-    });
-    cart.add({
-      product: product2,
-      quantity: 1,
-    });
-
-    cart.remove(product);
-
-    expect(cart.getTotal()).toEqual(product2.price);
   });
 });
