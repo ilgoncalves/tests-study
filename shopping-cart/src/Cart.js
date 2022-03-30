5a873b46aa7115482bc1b7399d50146dcda015cb
ilgoncalves <lima-igor@hotmail.com>
2022-03-29 23:00:42 -0300

    refactor: create utils to discount functions

--- shopping-cart/src/Cart.js
+++ shopping-cart/src/Cart.js
@@ -1,42 +1,9 @@
 import Money from "dinero.js";
-
-const getPercentage = (value, percentage) => {
-  return (value * percentage) / 100;
-};
-
-const calculateEachCondition = (amount, item, condition) => {
-  const productPrice = item.product.price;
-  const { percentage, minimumItems, quantity } = condition;
-  if (quantity) {
-    const rest = item.quantity % quantity;
-    return Money({
-      amount: ((item.quantity - rest) / quantity) * productPrice,
-    });
-  }
-
-  return Money({
-    amount:
-      item.quantity >= minimumItems ? getPercentage(amount, percentage) : 0,
-  });
-};
-
-const calculateDiscount = (amount, item) => {
-  let discount = Money({ amount: 0 });
-  if (item.condition) {
-    const { condition } = item;
-
-    const conditions = Array.isArray(condition) ? condition : [condition];
-    discount = Money({
-      amount: conditions
-        .map((el) => calculateEachCondition(amount, item, el).getAmount())
-        .sort((a, b) => b - a)[0],
-    });
-  }
-  return discount;
-};
-
 Money.defaultCurrency = "BRL";
 Money.defaultPrecision = 2;
+
+import { calculateDiscount } from "./discounts.utils";
+
 export default class Cart {
   items = [];
 
